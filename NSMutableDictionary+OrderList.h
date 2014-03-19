ccf9d654bfbb03cf5e4f56a63511d09c39519c44
刘澈 <liuche@localhost.(none)>
2014-03-19 15:20:09 +0800

    First commit

---
+++ NSMutableDictionary+OrderList.h
@@ -0,0 +1,25 @@
+//
+//  NSMutableDictionary+OrderList.h
+//  TestRunLoop
+//
+//  Created by 刘澈 on 14-3-19.
+//  Copyright (c) 2014年 刘澈. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import  <objc/runtime.h>
+
+@interface NSMutableDictionary (OrderList)
+
+
+-(void)putObject:(id)anObject forKey:(id<NSCopying>)aKey;
+
+-(NSArray *)getOrderAllKeys;
+
+-(id)getObjectForKey:(id)aKey;
+
+-(BOOL)deleteObjectForKey:(id)aKey;
+
+-(BOOL)deleteAllKeys;
+
+@end
---
+++ NSMutableDictionary+OrderList.m
@@ -0,0 +1,73 @@
+//
+//  NSMutableDictionary+OrderList.m
+//  TestRunLoop
+//
+//  Created by 刘澈 on 14-3-19.
+//  Copyright (c) 2014年 刘澈. All rights reserved.
+//
+
+#import "NSMutableDictionary+OrderList.h"
+
+@implementation NSMutableDictionary (OrderList)
+
+ const char OrderListKey;
+
+//order
+-(void)putObject:(id)anObject forKey:(id<NSCopying>)aKey{
+    NSMutableArray *orderList = [self getOrderList];
+    if(orderList == nil){
+        orderList = [[NSMutableArray alloc] init];
+    }
+    [self setObject:anObject forKey:aKey];
+    [orderList addObject:aKey];
+    [self setOrderList:orderList];
+}
+
+
+-(NSArray *)getOrderAllKeys{
+    NSMutableArray *orderList = [self getOrderList];
+    if(orderList !=nil && [orderList count]>0){
+        return [NSArray arrayWithArray:orderList];
+    }else{
+        return  nil;
+    }
+}
+
+
+-(id)getObjectForKey:(id)aKey{
+    return [self objectForKey:aKey];
+}
+
+-(void)setOrderList:(NSMutableArray *)orderList {
+    objc_setAssociatedObject(self,&OrderListKey,orderList,OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(NSMutableArray *)getOrderList{
+    NSMutableArray* orderList =objc_getAssociatedObject(self,&OrderListKey);
+    return orderList;
+}
+
+
+-(BOOL)deleteObjectForKey:(id)aKey{
+     NSMutableArray *orderList = [self getOrderList];
+    if(orderList != nil && [orderList containsObject:aKey]){
+        [self removeObjectForKey:aKey];
+        [orderList removeObject:aKey];
+        return  YES;
+    }else{
+        return NO;
+    }
+}
+
+-(BOOL)deleteAllKeys{
+    NSMutableArray *orderList = [self getOrderList];
+    if(orderList !=nil){
+        [self removeAllObjects];
+        [orderList removeAllObjects];
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+@end
