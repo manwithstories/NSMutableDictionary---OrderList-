b4921d73ff2490106af3ea36ec8aaaa164336785
刘澈 <liuche.java@gmail.com>
2014-03-26 16:30:49 +0800

    Update NSMutableDictionary+OrderList.h

--- NSMutableDictionary+OrderList.h
+++ NSMutableDictionary+OrderList.h
@@ -6,16 +6,22 @@
 
 @interface NSMutableDictionary (OrderList)
 
+/* 调用这个方法动态绑定属性，实现KEY的有序性 */
 -(void)putObject:(id)anObject forKey:(id<NSCopying>)aKey;
 
+/* 按照插入Key的顺序得到 key的有序列表 */
 -(NSArray *)getOrderAllKeys;
 
+/* 通过KEY 获得Value对象 */
 -(id)getObjectForKey:(id)aKey;
 
+/* 通过Key删除一个对象，同时也删除动态属性中维护的KEY */
 -(BOOL)deleteObjectForKey:(id)aKey;
 
+/* 删除字典中全部的对象，同时也删除动态属性全部的对象 */
 -(BOOL)deleteAllKeys;
 
+/*按照插入Key的顺序获得一个对象，没有则返回nil */
 -(id)getObjectForIndex:(NSUInteger)index;
 
 @end
